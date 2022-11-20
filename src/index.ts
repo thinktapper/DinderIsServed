17f587c31a20a792e0e7ca76c323070fc51512b7
Andrew Tapper <andrew@tapper.codes>
2022-11-20 03:54:13 -0600

    :passport_control: add auth module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
+
 import app from './server'
 
 const port = process.env.PORT || 3001
---
+++ src/modules/auth.ts
@@ -0,0 +1,46 @@
+import jwt from 'jsonwebtoken'
+import * as bcrypt from 'bcrypt'
+
+const salt = bcrypt.genSaltSync(10)
+
+export const comparePwds = (password, hash) => {
+  return bcrypt.compareSync(password, hash)
+}
+
+export const hashPwd = (password) => {
+  return bcrypt.hashSync(password, salt)
+}
+
+export const createJWT = (user) => {
+  const token = jwt.sign(
+    { id: user.id, username: user.username },
+    process.env.JWT_SECRET,
+  )
+  return token
+}
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization
+  if (!bearer) {
+    res.json({ ok: false, message: 'Not authorized' })
+    return
+  }
+
+  const [, token] = bearer.split(' ')
+  if (!token) {
+    res.json({ ok: false, message: 'Not valid' })
+    return
+  }
+
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = payload
+    console.log(payload)
+    next()
+    return
+  } catch (err) {
+    console.error(err)
+    res.json({ ok: false, message: 'Not valid' })
+    return
+  }
+}
